ff8a988218533347266c922e89f617125e9666e8
tuomjar <jarmo.tuomaala@gmail.com>
2025-05-25 19:53:26 +0300

    Lisätty mochawesome-raportointi GitHub Actionsiin

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -1,7 +1,22 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  video: false, // Estää videotallennuksen
   e2e: {
     baseUrl: 'http://localhost:5173',
+  },
+})
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:5173',
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: false,
+      html: false,
+      json: true
+    },
   },
 })
